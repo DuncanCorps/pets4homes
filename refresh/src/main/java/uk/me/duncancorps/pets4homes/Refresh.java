3ec4e9239d8d149abd7e30a5ec5e9ca670b687b3
Duncan Corps <dc@mercury.duncan-corps.me.uk>
2016-01-31 16:33:34 +0000

    split up overlong main(String[])

--- refresh/src/main/java/uk/me/duncancorps/pets4homes/Refresh.java
+++ refresh/src/main/java/uk/me/duncancorps/pets4homes/Refresh.java
@@ -1,8 +1,9 @@
-package uk.me.duncancorps;
+package uk.me.duncancorps.pets4homes;
 
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
@@ -12,14 +13,12 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import com.google.common.collect.Sets;
 
-public class Pets4Homes {
-	private static List<WebElement> finds(final SearchContext searchContext,
-			final By by) {
+public class Refresh {
+	private static List<WebElement> finds(final SearchContext searchContext, final By by) {
 		return searchContext.findElements(by);
 	}
 
-	private static WebElement find(final SearchContext searchContext,
-			final By by) {
+	private static WebElement find(final SearchContext searchContext, final By by) {
 		return searchContext.findElement(by);
 	}
 
@@ -29,62 +28,73 @@ public class Pets4Homes {
 		return StringUtils.equals(expectedUrl, actualUrl);
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		WebDriver webDriver = new HtmlUnitDriver(); // FirefoxDriver();
-		webDriver.get("http://www.pets4homes.co.uk/login/");
+	private static boolean logIn(WebDriver webDriver, String emailValue, String passValue) {
 		boolean loggedIn = false;
 		List<WebElement> forms = finds(webDriver, By.tagName("form"));
 
 		for (WebElement form : forms) {
 			if (!loggedIn) {
-				WebElement email = find(form, By.name("email"));
-				WebElement pass = find(form, By.name("pass"));
-				WebElement login = find(form, By.name("login"));
+				WebElement emailWebElement = find(form, By.name("email"));
+				WebElement passWebElement = find(form, By.name("pass"));
+				WebElement loginWebElement = find(form, By.name("login"));
 
-				if (email != null && pass != null && login != null) {
-					email.sendKeys("liz.corps@ntlworld.com");
-					pass.sendKeys("Canterbury1");
-					login.click();
+				if (emailWebElement != null && passWebElement != null && loginWebElement != null) {
+					emailWebElement.sendKeys(emailValue);
+					passWebElement.sendKeys(passValue);
+					loginWebElement.click();
 
-					loggedIn = currentUrlIs(webDriver,
-							"http://www.pets4homes.co.uk/account/");
+					loggedIn = currentUrlIs(webDriver, "http://www.pets4homes.co.uk/account/");
 				}
 			}
 		}
 
-		if (loggedIn) {
-			Set<String> aHrefs = Sets.newHashSet();
-			boolean clicked;
+		return loggedIn;
+	}
 
-			do {
-				clicked = false;
-				webDriver
-						.get("http://www.pets4homes.co.uk/account/manage-adverts/");
-				List<WebElement> as = finds(webDriver,
-						By.cssSelector("table.manageTable a"));
+	private static void refreshAdverts(WebDriver webDriver) {
+		Set<String> aHrefs = Sets.newHashSet();
+		boolean clicked;
 
-				for (WebElement a : as) {
-					if (!clicked) {
-						String aTitle = a.getAttribute("title");
+		do {
+			clicked = false;
+			webDriver.get("http://www.pets4homes.co.uk/account/manage-adverts/");
+			List<WebElement> aWebElements = finds(webDriver, By.cssSelector("table.manageTable a"));
 
-						if (StringUtils.equals(aTitle,
-								"Refreshes adverts last updated date")) {
-							String aHref = a.getAttribute("href");
+			for (WebElement aWebElement : aWebElements) {
+				if (!clicked) {
+					String aTitle = aWebElement.getAttribute("title");
 
-							if (!aHrefs.contains(aHref)) {
-								a.click();
-								aHrefs.add(aHref);
-								clicked = true;
-							}
+					if (StringUtils.equals(aTitle, "Refreshes adverts last updated date")) {
+						String aHref = aWebElement.getAttribute("href");
+
+						if (!aHrefs.contains(aHref)) {
+							aWebElement.click();
+							aHrefs.add(aHref);
+							clicked = true;
 						}
 					}
 				}
-			} while (clicked);
+			}
+		} while (clicked);
+	}
+
+	public static void main(String[] arguments) throws InterruptedException {
+		if (ArrayUtils.getLength(arguments) >= 2) {
+			String emailValue = "liz.corps@ntlworld.com";
+			String passValue = "Canterbury1";
+			WebDriver webDriver = new HtmlUnitDriver(); // FirefoxDriver();
+			webDriver.get("http://www.pets4homes.co.uk/login/");
+
+			final boolean loggedIn = logIn(webDriver, emailValue, passValue);
+
+			if (loggedIn) {
+				refreshAdverts(webDriver);
+			}
+
+			Thread.sleep(2000);
+			webDriver.close();
+			webDriver.quit();
+		} else {
 		}
-
-		Thread.sleep(2000);
-
-		webDriver.close();
-		webDriver.quit();
 	}
 }
